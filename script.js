14cdac6e47ef72e7d49d1fb4424f7263b878f17a
Hunter <hvanlear@gmail.com>
2022-05-19 13:57:45 -0400

    added

--- script.js
+++ script.js
@@ -5,11 +5,31 @@ let filteredByContainer = document.querySelector(".filtered-by-container");
 let resetBtn = document.querySelector(".btn-reset");
 menuOptions = document.querySelectorAll(".select-item");
 
+let dropdown = document.querySelector(".dropdown");
+let dropdownMenu = document.querySelector(".dropdown-menu");
+let filterLink = document.querySelector("#dropdownFilterLink");
+
+let cardBody = document.querySelector(".card-body");
+
 // for regular select list to keep dropdown from closing
 // $(".filter-item").on("click", function (event) {
 //   console.log(event);
 //   event.stopPropagation();
 // });
+
+filterLink.addEventListener("click", (e) => {
+  if (!dropdown.classList.contains("show")) {
+    filterLink.classList.add("btn-secondary-active");
+  } else if (dropdown.classList.contains("show")) {
+    filterLink.classList.remove("btn-secondary-active");
+  }
+});
+
+cardBody.addEventListener("click", (e) => {
+  if (!dropdown.classList.contains("show")) {
+    filterLink.classList.remove("btn-secondary-active");
+  }
+});
 
 let reloadPage = () => {
   location.reload();
@@ -19,11 +39,12 @@ const filteredBy = `<span class="filter-status-container ml-1"
 ><span class="filtered-start font-weight-bold"
   >Filtered By:
 </span>
-<span class="text-muted"> Status </span>
-|
-<span class="text-muted"> Owner </span>
-|
-<span class="text-primary"> Edit</span>
+<span class="text-muted ml-1"> Status </span>
+<span class="text-muted ml-1"> | </span>
+<span class="text-muted ml-1"> Category </span>
+<span class="text-muted ml-1"> | </span>
+<a class="text-primary ml-1 filter-edit" id="filterEdit"   role="button"
+> Edit</a>
 </span>`;
 
 $(document).ready(function () {
@@ -35,14 +56,21 @@ $(document).ready(function () {
   });
 });
 
-menuOptions.forEach((item) => {
-  item.addEventListener("click", (event) => {});
+document.addEventListener("click", (e) => {
+  if (e.target && e.target.id == "filterEdit") {
+    filterLink.classList.add("btn-secondary-active");
+    dropdownMenu.classList.add("show");
+    dropdownMenu.setAttribute(
+      "style",
+      "position: absolute; transform: translate3d(-28px, 38px, 0px); top: 0px; left: 0px; will-change: transform;"
+    );
+  }
 });
 
+let filterEdit = document.querySelector(".filter-edit");
+
 filterBtn.addEventListener("click", () => {
-  menuOptions.forEach((item) => {
-    // console.log(item.text);
-  });
+  filterLink.classList.remove("btn-secondary-active");
   filteredByContainer.innerHTML = filteredBy;
 });
 
